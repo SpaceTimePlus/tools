8e1968d4aefe66730fc317e584422473c8fa4e9a
liuhui <liuhui@weiwuu.net>
2018-09-16 00:29:09 +0800

    Init

--- src/main/java/fun/dodo/tools/help/StringUtil.java
+++ src/main/java/fun/dodo/tools/help/StringUtil.java
@@ -7,35 +7,36 @@ import java.util.Map;
 import java.util.Objects;
 
 public class StringUtil {
-  private StringUtil() {
-  }
-
-  public static boolean isNullOrEmpty(String value) {
-    return null == value || value.trim().isEmpty() || Objects.equals("null", value.trim().toLowerCase()) || Objects.equals("undefined", value.trim().toLowerCase());
-  }
-
-  public static String expressMultiMap(MultiMap multiMap) {
-    StringBuilder builder = new StringBuilder();
-    Iterator var2 = multiMap.iterator();
-
-    while(var2.hasNext()) {
-      Map.Entry<String, String> entry = (Map.Entry)var2.next();
-      builder.append((String)entry.getKey()).append(": ").append((String)entry.getValue()).append(", ");
+    private StringUtil() {
     }
 
-    return builder.toString();
-  }
-
-  public static String expressHashMap(Map hashMap) {
-    StringBuilder builder = new StringBuilder();
-    Iterator var2 = hashMap.entrySet().iterator();
-
-    while(var2.hasNext()) {
-      Object item = var2.next();
-      Map.Entry entry = (Map.Entry)item;
-      builder.append(entry.getKey()).append(": ").append(entry.getValue()).append(", ");
+    public static boolean isNullOrEmpty(String value) {
+        return null == value || value.trim().isEmpty() || Objects.equals("null", value.trim().toLowerCase()) || Objects.equals("undefined", value.trim().toLowerCase());
     }
 
-    return builder.toString();
-  }
+    public static String expressMultiMap(MultiMap multiMap) {
+        StringBuilder builder = new StringBuilder();
+        Iterator var2 = multiMap.iterator();
+
+        while (var2.hasNext()) {
+            Map.Entry<String, String> entry = (Map.Entry) var2.next();
+            builder.append((String) entry.getKey()).append(": ").append((String) entry.getValue()).append(", ");
+        }
+
+        return builder.toString();
+    }
+
+    public static String expressHashMap(Map hashMap) {
+        StringBuilder builder = new StringBuilder();
+        Iterator var2 = hashMap.entrySet().iterator();
+
+        while (var2.hasNext()) {
+            Object item = var2.next();
+            Map.Entry entry = (Map.Entry) item;
+            builder.append(entry.getKey()).append(": ").append(entry.getValue()).append(", ");
+        }
+
+        return builder.toString();
+    }
+
 }
